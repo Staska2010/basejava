5023d55bbe52848abbc7df2f6384e0a38a981530
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-19 17:24:09 +0300

    BaseJava intro task

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,62 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int numberOfResumes = 0;
+    int positionOfResumeInStorage = 0;
 
     void clear() {
+        for (int counter = 0; counter <= numberOfResumes; counter++) {
+            storage[counter] = null;
+        }
+        numberOfResumes = 0;
     }
 
     void save(Resume r) {
+        if (numberOfResumes < 9999) {
+            storage[numberOfResumes] = r;
+            numberOfResumes++;
+        } else {
+            System.out.println("Not enough space in storage");
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume findResume = null;
+        for (int counter = 0; counter < numberOfResumes; counter++) {
+            if (storage[counter].uuid.equals(uuid)) {
+                findResume = storage[counter];
+                positionOfResumeInStorage = counter;
+                break;
+            }
+        }
+        if (findResume == null) {
+            System.out.println("No such resume in storage: ");
+        }
+        return findResume;
     }
 
     void delete(String uuid) {
+        Resume deleteResume = get(uuid);
+        if (deleteResume != null) {
+            for (int counter = positionOfResumeInStorage; counter < numberOfResumes; counter++) {
+                storage[counter] = storage[counter + 1];
+            }
+            numberOfResumes--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, numberOfResumes);
     }
 
     int size() {
-        return 0;
+        return numberOfResumes;
     }
 }
